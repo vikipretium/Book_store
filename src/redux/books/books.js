b6824d68730907c1e3e29d5a1d7c99e1476b75c6
vikipretium <vighneshpasupathy@gmail.com>
2022-04-10 00:25:18 +0530

    add save, load

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,40 +1,58 @@
+import { saveBook, loadBooks, removeBook } from '../apiFunctions';
+
 const ADD_BOOK = 'Book_store/books/ADD_BOOK';
 const REMOVE_BOOK = 'Book_store/books/REMOVE_BOOK';
+const BOOK_ADDED = 'Book_store/Books/BOOK_ADDED';
+const BOOK_REMOVED = 'Book_store/Books/BOOK_REMOVED';
+const LOAD_BOOKS = 'Book_store/Books/LOAD_BOOKS';
+const BOOKS_LOADED = 'Book_store/Books/BOOKS_LOADED';
 
-export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
+export const addBook = (book) => (dispatch) => {
+  dispatch({ type: ADD_BOOK });
+  saveBook(book).then((status) => {
+    if (status === 201 || status === 200) {
+      dispatch({ type: BOOK_ADDED, payload: book });
+    }
+  });
+};
 
-export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: id });
+export const booksLoad = () => (dispatch) => {
+  dispatch({ type: LOAD_BOOKS });
+  loadBooks().then((books) =>
+    dispatch({
+      type: BOOKS_LOADED,
+      payload: books,
+    })
+  );
+};
+
+export const bookRemove = (index, id) => (dispatch) => {
+  dispatch({ type: REMOVE_BOOK });
+  removeBook(id).then((status) => {
+    if (status === 201 || status === 200)
+      dispatch({ type: BOOK_REMOVED, payload: index });
+  });
+};
 
 const initialState = {
-  books: [
-    {
-      id: 1,
-      title: 'book1',
-      author: 'author1',
-    },
-    {
-      id: 2,
-      title: 'book2',
-      author: 'author2',
-    },
-    {
-      id: 3,
-      title: 'book3',
-      author: 'author3',
-    },
-    {
-      id: 4,
-      title: 'book4',
-      author: 'author4',
-    },
-  ],
+  books: [],
 };
 
 const booksReducer = (state = initialState, action = {}) => {
   switch (action.type) {
-    case ADD_BOOK:
-      return { ...state, books: [...state.books, action.payload] };
+    case LOAD_BOOKS:
     case REMOVE_BOOK:
+    case ADD_BOOK:
+      return { ...state, waiting: true };
+    case BOOK_ADDED:
+      return {
+        ...state,
+        books: [...state.books, action.payload],
+        waiting: false,
+      };
+    case BOOKS_LOADED:
+      return { ...state, books: [...state.books, ...action.payload] };
+    case BOOK_REMOVED:
       return {
         ...state,
         books: [
