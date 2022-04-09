d5fd94fb07f60e083af1603b30e0c23d177cd5c7
vikipretium <vighneshpasupathy@gmail.com>
2022-04-09 21:55:54 +0530

    add use selector and use dispatch

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,30 @@
-import React from 'react';
 import Proptypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-export default function Book({ book }) {
+const Book = ({ author, title, bookIndex }) => {
+  const dispatch = useDispatch();
   return (
-    <div>
-      <p>{book.title}</p>
-      <p>{book.author}</p>
-      <button type="button">Remove</button>
-    </div>
+    <>
+      <p>{title}</p>
+      <p>{author}</p>
+      <button
+        book-index={bookIndex}
+        onClick={(e) => {
+          const index = Number(e.target.getAttribute('book-index'));
+          dispatch(removeBook(index));
+        }}
+        type="button"
+      >
+        Remove
+      </button>
+    </>
   );
-}
+};
 
 Book.propTypes = {
-  book: Proptypes.objectOf(Proptypes.string).isRequired,
+  author: Proptypes.string.isRequired,
+  title: Proptypes.string.isRequired,
+  bookIndex: Proptypes.number.isRequired,
 };
+export default Book;
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-export default function Categories() {
-  return <button type="button">Check status</button>;
-}
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
+
+const Categories = () => {
+  const dispatch = useDispatch();
+  const status = useSelector(
+    (state) => (state.categoriesReducer.status ? state.categoriesReducer.status : ''),
+    shallowEqual,
+  );
+  return (
+    <>
+      <button
+        onClick={() => dispatch(checkStatus('Under construction'))}
+        type="button"
+      >
+        Check status
+      </button>
+      <p>{status}</p>
+    </>
+  );
+};
+export default Categories;
