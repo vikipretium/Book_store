d9faafe4bcc4396dc825e160623c2feb90c15831
vikipretium <vighneshpasupathy@gmail.com>
2022-04-09 21:55:01 +0530

    add use dispatch

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-export default function AddBook() {
+const AddBook = ({ id }) => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
   return (
-    <>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const book = { id, title, author };
+        dispatch(addBook(book));
+        setTitle('');
+        setAuthor('');
+      }}
+    >
       <h3>ADD NEW BOOK</h3>
-      <form>
-        <input type="text" placeholder="Book Title" />
-        <input type="text" placeholder="Book Author" />
-        <button type="submit">Add Book</button>
-      </form>
-    </>
+      <input
+        type="text"
+        placeholder="Book Title"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        required
+      />
+      <input
+        type="text"
+        placeholder="Book Author"
+        onChange={(e) => setAuthor(e.target.value)}
+        value={author}
+        required
+      />
+      <button type="submit">Add Book</button>
+    </form>
   );
-}
+};
+AddBook.propTypes = {
+  id: PropTypes.number.isRequired,
+};
+export default AddBook;
