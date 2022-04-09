426c79b0bfc8a2de17f72977d39d4bb32a3408ab
vikipretium <vighneshpasupathy@gmail.com>
2022-04-09 21:52:50 +0530

    add browser router and provider

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { connect } from 'react-redux';
 import NavBar from './components/NavBar';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
-const books = [
-  {
-    id: 1,
-    genre: 'genre',
-    title: 'book title',
-    author: 'author',
-    status: 'complete',
-    currentChapter: 'Current chapter',
-    chapterNumber: 'Chapter #number',
-  },
-];
-
 const App = () => (
-  <div>
+  <BrowserRouter>
     <NavBar />
     <Routes>
-      <Route exact path="/" element={<Books books={books} />} />
-      <Route exact path="/categories" element={<Categories />} />
+      <Route exact path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
     </Routes>
-  </div>
+  </BrowserRouter>
 );
 
-export default connect(App);
+export default App;
