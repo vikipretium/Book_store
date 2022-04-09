5c54e5f92e6997763fa2d8240cafcfb20247367b
vikipretium <vighneshpasupathy@gmail.com>
2022-04-09 21:56:57 +0530

    add reducers

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -6,17 +6,41 @@ export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
 export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: id });
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      id: 1,
+      title: 'book1',
+      author: 'author1',
+    },
+    {
+      id: 2,
+      title: 'book2',
+      author: 'author2',
+    },
+    {
+      id: 3,
+      title: 'book3',
+      author: 'author3',
+    },
+    {
+      id: 4,
+      title: 'book4',
+      author: 'author4',
+    },
+  ],
 };
 
 const booksReducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case ADD_BOOK:
-      return { ...state, books: action.payload };
+      return { ...state, books: [...state.books, action.payload] };
     case REMOVE_BOOK:
       return {
         ...state,
-        books: state.books.filter((book) => book.id !== action.payload),
+        books: [
+          ...state.books.slice(0, action.payload),
+          ...state.books.slice(action.payload + 1),
+        ],
       };
     default:
       return state;
